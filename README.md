0160903b2f1b45354991ced05f48d6f571e88196
Kaixu <zhangkaixu@hotmail.com>
2012-06-24 13:14:42 +0800

    优化了eval

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ isan
 * 使用平均感知器算法 averaged perceptron
 * 使用增量方式解码 incremental decoding
 
+## 中文句法分析模块
+
+特点
+* 使用平均感知器
+* 增量方式解码， 时间复杂度 `O(n)`
--- isan/parsing/inc_dependency.py
+++ isan/parsing/inc_dependency.py
@@ -337,12 +337,32 @@ class Model :
         self.rreduce_weights=pickle.load(file)
         file.close()
         
+class Eval:
+    @staticmethod
+    def make_color(s):
+        return '\033[36;01m%s\033[1;m'%s #blue
+    def __init__(self):
+        self.std=0
+        self.cor=0
+        self.start_time=time.time()
+    def __call__(self,std_result,rst_result):
+        self.std+=len(std_result)
+        self.cor+=sum(1 if s==r else 0 for s,r in zip(std_result,rst_result))
+        pass
+    def print(self):
+        duration=time.time()-self.start_time
+        print("std:%d rst:%d 正确率:\033[32;01m%.4f\033[1;m 历时:%.2f 现时:%s"%(
+                self.std,
+                self.cor,
+                self.cor/self.std,
+                duration,
+                time.strftime("%H:%M:%S")))
 
 def train(model,training,iteration=10):
     stats=model
     for i in range(iteration):
-        std,cor=0,0
         otime=time.time()
+        eva=Eval()
         for ln,line in enumerate(open(training)):
             stats.step+=1
             line=line.strip()
@@ -354,37 +374,30 @@ def train(model,training,iteration=10):
             rst_actions=stats.decode(raw)
             rst_result=stats.actions_to_result(rst_actions)
             std_actions=stats.result_to_actions(std_result)
-            std+=len(std_result)
-            cor+=sum(1 if s==r else 0 for s,r in zip(std_result,rst_result))
+            eva(std_result,rst_result)
             if std_result!=rst_result:
                 stats.update(rst_actions,-1)
                 stats.update(std_actions,1)
-        print(i+1,cor/std,time.time()-otime)
+        print(i+1,end=' ')
+        eva.print()
 
 def test(model,test):
     stats=model
-    std,cor=0,0
+    eva=Eval()
     for ln,line in enumerate(open(test)):
         line=line.strip()
         sen=codec.decode(line)
         raw=[(word,tag) for word,tag,_,_ in sen]
         std_result=[x for _,_,x,_ in sen]
-        #print(raw)
         rst_actions=stats.decode(raw)
         rst_result=stats.actions_to_result(rst_actions)
         std_actions=stats.result_to_actions(std_result)
-        std+=len(std_result)
-        '''for i in range(len(raw)):
-            if raw[i][1]!='PU':
-                std+=1
-                if std_result[i]==rst_result[i]:cor+=1'''
-        cor+=sum(1 if s==r else 0 for s,r in zip(std_result,rst_result))
-        #print(cor/std)
+        eva(std_result,rst_result)
         '''print(raw)
         for ind,s,r in zip(range(len(sen)),std_result,rst_result):
             if s!=r:
                 print(raw[ind],raw[s],raw[r])'''
-    print(cor/std)
+    eva.print()
 def predict(model,cor=sys.stdin,output=sys.stdout):
     stats=model
     for line in cor:
