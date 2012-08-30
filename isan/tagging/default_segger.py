f6d28b8f6b9f47d06f26d290002325516c02c923
zhangkaixu <zhangkaixu@hotmail.com>
2012-08-30 21:05:34 +0800

    we do the rest, you do the best

---
+++ isan/tagging/default_segger.py
@@ -0,0 +1,61 @@
+from struct import Struct
+class Segger:
+
+    actions=['s','c']
+    stat_fmt=Struct('hcch')
+    init_stat=stat_fmt.pack(*(0,b'0',b'0',0))
+
+    def actions_to_result(self,actions,raw):
+        sen=[]
+        cache=''
+        for c,a in zip(raw,actions[1:]):
+            cache+=c
+            if a=='s':
+                sen.append(cache)
+                cache=''
+        if cache:
+            sen.append(cache)
+        return sen
+    def result_to_actions(self,y):
+        actions=['s']
+        for w in y:
+            for i in range(len(w)-1):
+                actions.append('c')
+            actions.append('s')
+        return actions
+
+
+
+    def gen_actions_and_stats(self,stat):
+        ind,last,_,wordl=self.stat_fmt.unpack(stat)
+        return [('s',self.stat_fmt.pack(ind+1,b's',last,1)),
+                ('c',self.stat_fmt.pack(ind+1,b'c',last,wordl+1))]
+
+    def set_raw(self,raw):
+        self.raw=raw
+        self.uni_chars=list(x.encode() for x in '###'+raw+'##')
+        self.bi_chars=[self.uni_chars[i]+self.uni_chars[i+1]
+                for i in range(len(self.uni_chars)-1)]
+
+    def gen_features(self,span):
+        span=self.stat_fmt.unpack(span)
+        raw=self.raw
+        uni_chars=self.uni_chars
+        bi_chars=self.bi_chars
+
+        c_ind=span[0]+2
+        ws_current=span[1]
+        ws_left=span[2]
+        w_current=raw[span[0]-span[3]:span[0]]
+        fv=[ 
+                b'0'+ws_current+ws_left,
+                b"1"+uni_chars[c_ind]+ws_current,
+                b"2"+uni_chars[c_ind+1]+ws_current,
+                b'3'+uni_chars[c_ind-1]+ws_current,
+                b"a"+bi_chars[c_ind]+ws_current,
+                b"b"+bi_chars[c_ind-1]+ws_current,
+                b"c"+bi_chars[c_ind+1]+ws_current,
+                b"d"+bi_chars[c_ind-2]+ws_current,
+                b"w"+w_current.encode(),
+                ]
+        return fv
--- isan/tagging/inc_segger.py
+++ isan/tagging/inc_segger.py
@@ -6,116 +6,47 @@ import isan.tagging.cws_codec as tagging_codec
 import isan.tagging.eval as tagging_eval
 import isan.common.perceptrons as perceptrons
 import isan.tagging.dfabeam as dfabeam
+import isan.tagging.default_segger as segger
 """
 一个增量搜索模式的中文分词模块
 """
 
-class Actions:
-    sep='s'
-    com='c'
-    def __init__(self):
-        self.actions=[self.sep,self.com]
-    def as_result(self,actions,raw):
-        sen=[]
-        cache=''
-        for c,a in zip(raw,actions[1:]):
-            cache+=c
-            if a==self.sep:
-                sen.append(cache)
-                cache=''
-        if cache:
-            sen.append(cache)
-        return sen
-    def from_result(self,y):
-        actions=[self.sep]
-        for w in y:
-            for i in range(len(w)-1):
-                actions.append(self.com)
-            actions.append(self.sep)
-        return actions
-class Stats(Actions):
-    def __init__(self):
-        self.fmt="hcch"
-        self.init=(0,b'|',b'|',0)
-        
-    def from_actions(self,actions):
-        stat=self.init
-        for action in actions:
-            yield stat
-            ind,last,_,wordl=stat
-            if action==self.sep:
-                stat=(ind+1,b's',last,1)
-            else:
-                stat=(ind+1,b'c',last,wordl+1)
-        yield stat
-    def gen_stats(self,stat):
-        ind,last,_,wordl=stat
-        return [(self.sep,(ind+1,b's',last,1)),
-                (self.com,(ind+1,b'c',last,wordl+1))]
-
-class Features:
-    def set_raw(self,raw):
-        self.raw=raw
-        self.uni_chars=list(x.encode() for x in '###'+raw+'##')
-        self.bi_chars=[self.uni_chars[i]+self.uni_chars[i+1]
-                for i in range(len(self.uni_chars)-1)]
-    def gen_features(self,span):
-        raw=self.raw
-        uni_chars=self.uni_chars
-        bi_chars=self.bi_chars
-
-        c_ind=span[0]+2
-        ws_current=span[1]
-        ws_left=span[2]
-        w_current=raw[span[0]-span[3]:span[0]]
-        fv=[ 
-                b'0'+ws_current+ws_left,
-                b"1"+uni_chars[c_ind]+ws_current,
-                b"2"+uni_chars[c_ind+1]+ws_current,
-                b'3'+uni_chars[c_ind-1]+ws_current,
-                b"a"+bi_chars[c_ind]+ws_current,
-                b"b"+bi_chars[c_ind-1]+ws_current,
-                b"c"+bi_chars[c_ind+1]+ws_current,
-                b"d"+bi_chars[c_ind-2]+ws_current,
-                b"w"+w_current.encode(),
-                ]
-        return fv
-
-
 
 class Segmentation_Space:
-    default_conf={
-            'actions':Actions(),
-            'stats':Stats(),
-            'features':Features(),
-            'beam_width':8,
-            }
-    def __init__(self,beam_width=8,conf=default_conf):
-        self.conf=self.default_conf
-        self.beam_width=self.conf['beam_width']
-        self.actions={a:{} for a in self.conf['actions'].actions}
+    def __init__(self,beam_width=8):
+        self.beam_width=beam_width
         self.link()
 
-    def link(self):
-        self.stat_fmt=struct.Struct(self.conf['stats'].fmt)
-        self.init=self.stat_fmt.pack(*self.conf['stats'].init)
-        self.actions_to_stats=self.conf['stats'].from_actions
-        self.gen_stats=self.conf['stats'].gen_stats
-        self.actions_to_result=self.conf['actions'].as_result
-        self.result_to_actions=self.conf['actions'].from_result
-        self.gen_features=self.conf['features'].gen_features
+    def link(self,segger=segger.Segger()):
+        self.segger=segger
+        func_list={
+                'actions': None,
+                'init_stat': None,
+                'stat_fmt_str':'hcch',
+                'actions_to_result': None,
+                'result_to_actions': None,
+                'gen_actions_and_stats': None,
+                'actions_to_stats':self._actions_to_stats,
+                'gen_features': None,
+                }
+        for func,dft_attr in func_list.items():
+            if hasattr(self.segger,func):
+                setattr(self,func,getattr(self.segger,func))
+            else:
+                setattr(self,func,dft_attr)
+                
+        if not hasattr(self,'weights'):
+            self.weights={a:{} for a in self.actions}
         self.dfabeam=dfabeam.new(
                 self.beam_width,
                 #None,
-                self.init,
+                self.init_stat,
                 #None,
-                lambda x: [(a,self.stat_fmt.pack(*k))
-                    for a,k in 
-                        self.gen_stats(self.stat_fmt.unpack(x))],
+                self.gen_actions_and_stats,
                 #None,
-                lambda x: self.gen_features(self.stat_fmt.unpack(x)),
+                self.gen_features,
                 )
-        for k,v in self.actions.items():
+        for k,v in self.weights.items():
             dfabeam.set_action(self.dfabeam,k,v)
 
 
@@ -123,16 +54,17 @@ class Segmentation_Space:
         self.actions_to_result=None
         self.result_to_actions=None
         self.actions_to_stats=None
-        self.gen_stats=None
+        self.gen_actions_and_stats=None
         self.gen_features=None
         self.stat_fmt=None
+        self.segger=None
 
     def __del__(self):
         dfabeam.delete(self.dfabeam)
 
     #特征相关
     def set_raw(self,raw):
-        self.conf['features'].set_raw(raw)
+        self.segger.set_raw(raw)
 
     ### 特征更新相关 
     def update(self,x,std_actions,rst_actions,step):
@@ -140,12 +72,11 @@ class Segmentation_Space:
         self._update_actions(rst_actions,-1,step)
     def _update_actions(self,actions,delta,step):
         for stat,action in zip(self.actions_to_stats(actions),actions,):
-            stat=self.stat_fmt.pack(*stat)
             dfabeam.update_action(self.dfabeam,stat,action,delta,step)
    
 
     def average(self,step):
-        for k,v in self.actions.items():
+        for k,v in self.weights.items():
             v.update(dfabeam.export_weights(self.dfabeam,step,k))
 
     def search(self,raw):
@@ -153,7 +84,16 @@ class Segmentation_Space:
         dfabeam.set_raw([self.dfabeam,raw])
         ret=dfabeam.search([self.dfabeam,len(raw)+1])
         return ret
+    
 
+    def _actions_to_stats(self,actions):
+        stat=self.init_stat
+        for action in actions:
+            yield stat
+            for a,s in self.gen_actions_and_stats(stat):
+                if action==a:
+                    stat=s
+        yield stat
 
 class Model(perceptrons.Base_Model):
     """
