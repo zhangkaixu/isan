99bd63d737024040aa59b5f48fff03b2f00c82f3
zhangkaixu <zhangkaixu@hotmail.com>
2012-09-04 16:52:32 +0800

    prepare for perceptron.py

--- isan/common/perceptrons.py
+++ isan/common/perceptrons.py
@@ -1,117 +1,6 @@
 #!/usr/bin/python3
 import pickle
 import collections
-class Weights(dict):
-    """
-    感知器特征的权重
-    """
-    def __init__(self):
-        self.acc=collections.defaultdict(int)
-    def update(self,feature,delta=0,step=0):
-        self.setdefault(feature,0)
-        self[feature]+=delta
-        self.acc[feature]+=step*delta
-    def __call__(self,fv):
-        #return 0
-        return sum(self.get(x,0)for x in fv)
-        #return sum(map(lambda x:self.get(x,0),fv))
-    def updates(self,features,delta=0,step=0):
-        for feature in features:
-            self.setdefault(feature,0)
-            self[feature]+=delta
-            self.acc[feature]+=step*delta
-    def average(self,step):
-        for k in self.acc:
-            self[k]=(self[k]-self.acc[k]/step)
-            if self[k]==0:del self[k]
-        del self.acc
-
-class Base_Decoder(object):
-    """
-    解码
-
-    self.sequence: 动作序列的beam
-    
-    需要实现：
-    self.gen_next(ind,stat) # 第ind个动作，当前状态为stat时候，产生后续状态
-
-    """
-    def __init__(self,beam_width):
-        self.beam_width=beam_width#搜索柱宽度
-    def thrink(self,ind):
-        #找到最好的alphas
-        for k,v in self.sequence[ind].items():
-            #这里使用排序比使用max还快，但保留max的代码
-            #alphas=v['alphas']
-            #max_ind=max(enumerate(v['alphas']),key=lambda x:x[1])[0]
-            #alphas[0],alphas[max_ind]=alphas[max_ind],alphas[0]
-            
-            v['alphas'].sort(reverse=True,key=lambda x:x[0])
-        #构造beam
-        beam=sorted(list(self.sequence[ind].items()),key=lambda x:x[1]['alphas'][0][0],reverse=True)
-        #print(len(beam))
-        beam=beam[:min(len(beam),self.beam_width)]
-        #print(len(beam))
-        return [stat for stat,_ in beam]
-    def forward(self,get_step=lambda x:len(x)+1):
-        #前向搜索
-        self.sequence[0][self.stats.init]=dict(self.init_data)#初始化第一个状态
-        for ind in range(get_step(self.raw)):
-            for stat in self.thrink(ind):
-                self.gen_next(ind,stat)
-    def backward(self):
-        """
-        使用beta算法计算后向分数
-        """
-        sequence=self.sequence
-        ind=len(sequence)-1
-        for stat,alpha_beta in sequence[ind].items():#初始化最后一项的分数
-            alpha_beta[1].append((0,None,None,None))
-        while ind>0:
-            for stat,alpha_beta in sequence[ind].items():
-                alphas=alpha_beta[0]
-                if not alpha_beta[1]: continue
-                beta=alpha_beta[1][0][0]
-                for score,delta,action,pre_stat in alphas:
-                    sequence[ind-1][pre_stat][1].append((beta+delta,delta,action,stat))
-            #排序
-            for _,alpha_beta in sequence[ind-1].items():
-                alpha_beta[1].sort(reverse=True)
-            ind-=1
-
-class Base_Stats(object):
-    """
-    需要实现
-    self.init 初始状态
-    self.gen_next_stats
-    self._actions_to_stats
-    """
-    def update(self,x,std_actions,rst_actions,step,sequence=None):
-        #print('std',std_actions)
-        #print('rst',rst_actions)
-        #print("begin")
-        length=self._update_actions(std_actions,1,step,sequence)
-        #print(len(sequence),length)
-        length=self._update_actions(rst_actions,-1,step,sequence[:length])
-        #print(length)
-        #print("end")
-    ### 私有函数 
-    def _update_actions(self,actions,delta,step,sequence):
-        length=0
-        #print(len(actions),len(sequence))
-        for stat,action,beam in zip(self._actions_to_stats(actions),actions,sequence[:]):
-            fv=self.features(stat)
-            if action not in self.actions:
-                #print(action)
-                self.actions.new_action(action)
-            self.actions[action].updates(fv,delta,step)
-            length+=1
-            #print('stat',stat)
-            if stat not in beam:
-                #print('early update',length)
-                #return length
-                pass
-        return length
 
 class Base_Model(object):
     def __init__(self,model_file,schema=None,**conf):
@@ -124,18 +13,17 @@ class Base_Model(object):
             file=open(model_file,"rb")
             self.schema=pickle.load(file)
             file.close()
+            self.schema.link()
         else:
             self.model_file=model_file
             self.schema=schema
-        self.actions=self.schema.actions
-        self.stats=self.schema.stats
         self.step=0
 
     def test(self,test_file):
         """
         测试
         """
-        eval=self.Eval()
+        eval=self.schema.Eval()
         for line in open(test_file):
             raw,y,set_Y=self.codec.decode(line.strip())
             hat_y=self(raw)
@@ -145,7 +33,7 @@ class Base_Model(object):
         """
         保存模型
         """
-        self.actions.average(self.step)
+        self.schema.average(self.step)
         file=open(self.model_file,'wb')
         pickle.dump(self.schema,file)
         file.close()
@@ -154,7 +42,7 @@ class Base_Model(object):
         解码，读入生句子，返回词的数组
         """
         rst_actions=self.schema.search(raw)
-        hat_y=self.actions.actions_to_result(rst_actions,raw)
+        hat_y=self.schema.actions_to_result(rst_actions,raw)
         return hat_y
     def _learn_sentence(self,raw,y,set_Y=None):
         """
@@ -162,13 +50,13 @@ class Base_Model(object):
         """
         self.step+=1#学习步数加一
         if y:
-            std_actions=self.actions.result_to_actions(y)#得到标准动作
+            std_actions=self.schema.result_to_actions(y)#得到标准动作
         else:
             std_actions=self.schema.search(raw,set_Y)
         rst_actions=self.schema.search(raw)#得到解码后动作
-        hat_y=self.actions.actions_to_result(rst_actions,raw)#得到解码后结果
+        hat_y=self.schema.actions_to_result(rst_actions,raw)#得到解码后结果
         if y!=hat_y:#如果动作不一致，则更新
-            self.stats.update(raw,std_actions,rst_actions,self.step,self.schema.sequence)
+            self.schema.update(raw,std_actions,rst_actions,self.step,self.schema.sequence)
         return y,hat_y
         
     def train(self,training_file,iteration=5):
@@ -176,7 +64,7 @@ class Base_Model(object):
         训练
         """
         for it in range(iteration):#迭代整个语料库
-            eval=self.Eval()#测试用的对象
+            eval=self.schema.Eval()#测试用的对象
             if type(training_file)==str:training_file=[training_file]
             for t_file in training_file:
                 for line in open(t_file):#迭代每个句子
--- isan/parsing/inc_dep.py
+++ isan/parsing/inc_dep.py
@@ -5,13 +5,186 @@ import isan.parsing.dep_codec as codec
 import isan.parsing.eval as eval
 import isan.common.perceptrons as perceptrons
 
-class Default_Features :
+
+class Weights(dict):
+    """
+    感知器特征的权重
+    """
+    def __init__(self):
+        self.acc=collections.defaultdict(int)
+    def update(self,feature,delta=0,step=0):
+        self.setdefault(feature,0)
+        self[feature]+=delta
+        self.acc[feature]+=step*delta
+    def __call__(self,fv):
+        #return 0
+        return sum(self.get(x,0)for x in fv)
+        #return sum(map(lambda x:self.get(x,0),fv))
+    def updates(self,features,delta=0,step=0):
+        for feature in features:
+            self.setdefault(feature,0)
+            self[feature]+=delta
+            self.acc[feature]+=step*delta
+    def average(self,step):
+        for k in self.acc:
+            self[k]=(self[k]-self.acc[k]/step)
+            if self[k]==0:del self[k]
+        del self.acc
+
+class Stats :
+    def __init__(self):
+        #初始状态 (解析位置，上一个位置结果，上上个位置结果，当前词长)
+        self.init=(0,(0,0),(None,None,None))
+    def shift(self,raw,stat):
+        ind,_,stack_top=stat
+        if ind>=len(raw): return None
+        return (ind+1,
+                (ind,ind+1),
+                ((raw[ind][0],raw[ind][1],None,None),
+                        stack_top[0],
+                        stack_top[1][1] if stack_top[1] else None)
+                )
+    def reduce(self,raw,stat,predictor):
+        ind,span,stack_top=stat
+        _,p_span,_=predictor
+        s0,s1,s2=stack_top
+        if s0==None or s1==None:return None,None
+        return ((ind,
+                (p_span[0],span[1]),
+                ((s1[0],s1[1],s1[2],s0[1]),predictor[2][1],predictor[2][2])),
+             (ind,
+                (p_span[0],span[1]),
+                ((s0[0],s0[1],s1[1],s0[3]),predictor[2][1],predictor[2][2])),)
+    def gen_next_stats(self,stat):
+        #print(stat)
+        yield 's',self.shift(self.raw,stat)
+        l,r=self.reduce(self.raw,stat)
+        yield 'l',l
+        yield 'r',r
+    def back_gen_next_stats(self,stat):
+        """
+        由现有状态产生合法新状态
+        """
+        ind,last,_,wordl=stat
+        yield 's',(ind+1,'s',last,1)
+        yield 'c',(ind+1,'c',last,wordl+1)
+
+class Decoder:
+    """
+    线性搜索
+    value = [alphas,betas]
+    alpha = [score, delta, action, link]
+    """
+    def thrink(self,ind):
+        #找到最好的alphas
+        for k,v in self.sequence[ind].items():
+            #这里使用排序比使用max还快，但保留max的代码
+            #alphas=v['alphas']
+            #max_ind=max(enumerate(v['alphas']),key=lambda x:x[1])[0]
+            #alphas[0],alphas[max_ind]=alphas[max_ind],alphas[0]
+            
+            v['alphas'].sort(reverse=True,key=lambda x:x[0])
+        #构造beam
+        beam=sorted(list(self.sequence[ind].items()),key=lambda x:x[1]['alphas'][0][0],reverse=True)
+        #print(len(beam))
+        beam=beam[:min(len(beam),self.beam_width)]
+        #print(len(beam))
+        return [stat for stat,_ in beam]
+    def forward(self,get_step=lambda x:len(x)+1):
+        #前向搜索
+        self.sequence[0][self.stats.init]=dict(self.init_data)#初始化第一个状态
+        for ind in range(get_step(self.raw)):
+            for stat in self.thrink(ind):
+                self.gen_next(ind,stat)
+    def backward(self):
+        """
+        使用beta算法计算后向分数
+        """
+        sequence=self.sequence
+        ind=len(sequence)-1
+        for stat,alpha_beta in sequence[ind].items():#初始化最后一项的分数
+            alpha_beta[1].append((0,None,None,None))
+        while ind>0:
+            for stat,alpha_beta in sequence[ind].items():
+                alphas=alpha_beta[0]
+                if not alpha_beta[1]: continue
+                beta=alpha_beta[1][0][0]
+                for score,delta,action,pre_stat in alphas:
+                    sequence[ind-1][pre_stat][1].append((beta+delta,delta,action,stat))
+            #排序
+            for _,alpha_beta in sequence[ind-1].items():
+                alpha_beta[1].sort(reverse=True)
+            ind-=1
+
+    def new_action(self,action):
+        self.actions[action]=perceptrons.Weights()
+    def _actions_to_stats(self,actions):
+        #print(actions)
+        sn=sum(1 if a=='s' else 0 for a in actions)
+        assert(sn*2-1==len(actions))
+        stat=None
+        stack=[]
+        ind=0
+        for action in actions:
+            stat=(ind,(0,0),(tuple(stack[-1]) if len(stack)>0 else None,
+                        tuple(stack[-2]) if len(stack)>1 else None,
+                        tuple(stack[-3][1]) if len(stack)>2 else None,
+                        ))
+            yield stat
+            if action=='s':
+                stack.append([self.raw[ind][0],self.raw[ind][1],None,None])
+                ind+=1
+            else:
+                left=stack[-2][0]
+                right=stack[-1][1]
+                if action=='l':
+                    stack[-2][3]=stack[-1][1]
+                    stack.pop()
+                if action=='r':
+                    stack[-1][2]=stack[-2][1]
+                    stack[-2]=stack[-1]
+                    stack.pop()
+    def update(self,x,std_actions,rst_actions,step,sequence=None):
+        length=self._update_actions(std_actions,1,step,sequence)
+        length=self._update_actions(rst_actions,-1,step,sequence[:length])
+    ### 私有函数 
+    def _update_actions(self,actions,delta,step,sequence):
+        length=0
+        for stat,action,beam in zip(self._actions_to_stats(actions),actions,sequence[:]):
+            fv=self.gen_features(stat)
+            if action not in self.actions:
+                self.actions.new_action(action)
+            self.actions[action].updates(fv,delta,step)
+            length+=1
+            if stat not in beam:
+                pass
+        return length
+    
+    def __init__(self,beam_width=8):
+        self.beam_width=beam_width#搜索柱宽度
+        self.init_data={'pi':set(),
+                    'alphas':[{0 : 0, 'v' : 0, 'a': None }]}#初始状态
+        self.stats=Stats()
+        self.actions={}
+        
+        self.actions['s']=Weights()#特征
+        self.actions['l']=Weights()#特征
+        self.actions['r']=Weights()#特征
+    
+    Eval=eval.Eval
+    def link(self):
+        pass
+    def unlink(self):
+        pass
+    def average(self,steps):
+        for v in self.actions.values():
+            v.average(steps)
     def set_raw(self,raw):
         """
         对需要处理的句子做必要的预处理（如缓存特征）
         """
         self.raw=raw
-    def __call__(self,stat):
+    def gen_features(self,stat):
         ind,_,stack_top=stat
         s0,s1,s2_t=stack_top
         s0_w,s0_t,s0l_t,s0r_t=None,None,None,None
@@ -38,8 +211,101 @@ class Default_Features :
             ('s0ts1ts2t',s0_t,s1_t,s2_t),
                 ]
         return fv
+    def _find_result(self,step,stat,begin,end,actions,stats):
+        if begin==end: return
+        info=self.sequence[step][stat]
+        alpha=info['alphas'][0]
+        action=alpha['a']
+        actions[end-1]=alpha['a']
+        if alpha['a']=='s': 
+            last_ind,last_stat=alpha['p'][0]
+            stats[last_ind]=last_stat
+            return
+        last,reduced=alpha['p']
+        r_ind,r_stat=reduced
+        last_ind,last_stat=last
+        actions[r_ind]='s'
+        stats[last_ind]=last_stat
+        stats[r_ind]=r_stat
+        self._find_result(r_ind,r_stat,begin,r_ind,actions,stats)
+        self._find_result(last_ind,last_stat,r_ind+1,end-1,actions,stats)
 
-class Actions(dict):
+    def search(self,raw):
+        self.raw=raw
+        self.stats.raw=raw
+        self.set_raw(raw)
+        self.sequence=[{} for i in range(2*len(raw))]
+        #self.sequence[0][self.stats.init]={'pi':set(),
+        #            'alphas':[{'c' : 0, 'v' : 0, 'a': None }]}#初始状态
+        self.forward(lambda x:2*len(x)-1)
+        res=self.make_result()
+        return res
+
+    def gen_next(self,ind,stat):
+        """
+        根据第ind步的状态stat，产生新状态，并计算data
+        """
+        fv=self.gen_features(stat)#得到当前状态的特征向量
+
+        stat_info=self.sequence[ind][stat]
+        alphas=stat_info['alphas']
+        #betas=stat_info.setdefault('betas',{})
+        predictors=stat_info['pi']
+        c,v=alphas[0][0],alphas[0]['v']
+        #shift
+        key=self.stats.shift(self.raw,stat)
+        if key:
+            if key not in self.sequence[ind+1]:
+                self.sequence[ind+1][key]={'pi':set(),'alphas':[]}
+            new_stat_info=self.sequence[ind+1][key]
+            new_stat_info['pi'].add((ind,stat))
+            #xi=self.shift_weights(fv)
+            xi=self.actions['s'](fv)
+            new_stat_info['alphas'].append({
+                        0:c+xi,
+                        'v':0,
+                        'a':'s',
+                        'p':((ind,stat),)})
+            #betas['s']=[key]
+        for p_step,predictor in predictors:
+            last_stat_info=self.sequence[p_step][predictor]
+            last_fv=self.gen_features(predictor)
+            #last_xi=self.shift_weights(last_fv)
+            last_xi=self.actions['s'](last_fv)
+            last_c,last_v=last_stat_info['alphas'][0][0],last_stat_info['alphas'][0]['v']
+            lkey,rkey=self.stats.reduce(self.raw,stat,predictor)
+            #left-reduce
+            if lkey :
+                if lkey not in self.sequence[ind+1]:
+                    self.sequence[ind+1][lkey]={'pi':set(),'alphas':[]}
+                new_stat_info=self.sequence[ind+1][lkey]
+                new_stat_info['pi'].update(last_stat_info['pi'])
+                #lamda=self.lreduce_weights(fv)
+                lamda=self.actions['l'](fv)
+                delta=lamda+last_xi
+                new_stat_info['alphas'].append({
+                            0:last_c+v+delta,
+                            'v':last_v+v+delta,
+                            'a':'l',
+                            'p':((ind,stat),(p_step,predictor))})
+                #betas['l']=[lkey]
+
+            #right-reduce
+            if rkey:
+                if rkey not in self.sequence[ind+1]:
+                    self.sequence[ind+1][rkey]={'pi':set(),'alphas':[]}
+                new_stat_info=self.sequence[ind+1][rkey]
+                new_stat_info['pi'].update(last_stat_info['pi'])
+                #rho=self.rreduce_weights(fv)
+                rho=self.actions['r'](fv)
+                delta=rho+last_xi
+                new_stat_info['alphas'].append({
+                            0:last_c+v+delta,
+                            'v':last_v+v+delta,
+                            'a':'r',
+                            'p':((ind,stat),(p_step,predictor))})
+                #betas['r']=[rkey]
+
     @staticmethod
     def actions_to_result(actions,raw):
         #print(actions,raw)
@@ -110,218 +376,6 @@ class Actions(dict):
         assert(len(actions)==2*len(result)-1)
         return actions
 
-    def __init__(self):
-        self['s']=perceptrons.Weights()#特征
-        self['l']=perceptrons.Weights()#特征
-        self['r']=perceptrons.Weights()#特征
-
-    def average(self,step):
-        for v in self.values():
-            v.average(step)
-    def new_action(self,action):
-        self[action]=perceptrons.Weights()
-class Stats(perceptrons.Base_Stats):
-    def __init__(self,actions,features):
-        self.actions=actions
-        self.features=features
-        #初始状态 (解析位置，上一个位置结果，上上个位置结果，当前词长)
-        self.init=(0,(0,0),(None,None,None))
-    def shift(self,raw,stat):
-        ind,_,stack_top=stat
-        if ind>=len(raw): return None
-        return (ind+1,
-                (ind,ind+1),
-                ((raw[ind][0],raw[ind][1],None,None),
-                        stack_top[0],
-                        stack_top[1][1] if stack_top[1] else None)
-                )
-    def reduce(self,raw,stat,predictor):
-        ind,span,stack_top=stat
-        _,p_span,_=predictor
-        s0,s1,s2=stack_top
-        if s0==None or s1==None:return None,None
-        return ((ind,
-                (p_span[0],span[1]),
-                ((s1[0],s1[1],s1[2],s0[1]),predictor[2][1],predictor[2][2])),
-             (ind,
-                (p_span[0],span[1]),
-                ((s0[0],s0[1],s1[1],s0[3]),predictor[2][1],predictor[2][2])),)
-    def gen_next_stats(self,stat):
-        #print(stat)
-        yield 's',self.shift(self.raw,stat)
-        l,r=self.reduce(self.raw,stat)
-        yield 'l',l
-        yield 'r',r
-    def back_gen_next_stats(self,stat):
-        """
-        由现有状态产生合法新状态
-        """
-        ind,last,_,wordl=stat
-        yield 's',(ind+1,'s',last,1)
-        yield 'c',(ind+1,'c',last,wordl+1)
-
-    def _actions_to_stats(self,actions):
-        #print(actions)
-        sn=sum(1 if a=='s' else 0 for a in actions)
-        assert(sn*2-1==len(actions))
-        stat=None
-        stack=[]
-        ind=0
-        for action in actions:
-            stat=(ind,(0,0),(tuple(stack[-1]) if len(stack)>0 else None,
-                        tuple(stack[-2]) if len(stack)>1 else None,
-                        tuple(stack[-3][1]) if len(stack)>2 else None,
-                        ))
-            #print('stat',stat)
-            yield stat
-            if action=='s':
-                stack.append([self.raw[ind][0],self.raw[ind][1],None,None])
-                ind+=1
-            else:
-                left=stack[-2][0]
-                right=stack[-1][1]
-                if action=='l':
-                    stack[-2][3]=stack[-1][1]
-                    stack.pop()
-                if action=='r':
-                    stack[-1][2]=stack[-2][1]
-                    stack[-2]=stack[-1]
-                    stack.pop()
-                    
-        #stat=self.init
-        #for action in actions:
-        #    yield stat
-        #    ind,last,_,wordl=stat
-        #    if action=='s':
-        #        stat=(ind+1,'s',last,1)
-        #    else:
-        #        stat=(ind+1,'c',last,wordl+1)
-        #yield stat
-class Decoder(perceptrons.Base_Decoder):
-    """
-    线性搜索
-    value = [alphas,betas]
-    alpha = [score, delta, action, link]
-    """
-    def debug(self):
-        """
-        used to generate lattice
-        """
-        self.searcher.backward()
-        sequence=self.searcher.sequence
-        for i,d in enumerate(sequence):
-            for stat,alpha_beta in d.items():
-                if alpha_beta[1]:
-                    for beta,db,action,n_stat in alpha_beta[1]:
-                        if beta==None:continue
-                        delta=alpha_beta[0][0][0]+beta-self.searcher.best_score
-                        if action=='s':
-                            pass
-    
-    def __init__(self,beam_width=8):
-        super(Decoder,self).__init__(beam_width)
-        self.init_data={'pi':set(),
-                    'alphas':[{0 : 0, 'v' : 0, 'a': None }]}#初始状态
-        self.features=Default_Features()
-        self.actions=Actions()
-        self.stats=Stats(self.actions,self.features)
-    
-    def _find_result(self,step,stat,begin,end,actions,stats):
-        if begin==end: return
-        info=self.sequence[step][stat]
-        alpha=info['alphas'][0]
-        action=alpha['a']
-        actions[end-1]=alpha['a']
-        if alpha['a']=='s': 
-            last_ind,last_stat=alpha['p'][0]
-            stats[last_ind]=last_stat
-            return
-        last,reduced=alpha['p']
-        r_ind,r_stat=reduced
-        last_ind,last_stat=last
-        actions[r_ind]='s'
-        stats[last_ind]=last_stat
-        stats[r_ind]=r_stat
-        self._find_result(r_ind,r_stat,begin,r_ind,actions,stats)
-        self._find_result(last_ind,last_stat,r_ind+1,end-1,actions,stats)
-
-    def search(self,raw):
-        self.raw=raw
-        self.stats.raw=raw
-        self.features.set_raw(raw)
-        self.sequence=[{} for i in range(2*len(raw))]
-        #self.sequence[0][self.stats.init]={'pi':set(),
-        #            'alphas':[{'c' : 0, 'v' : 0, 'a': None }]}#初始状态
-        self.forward(lambda x:2*len(x)-1)
-        res=self.make_result()
-        return res
-
-    def gen_next(self,ind,stat):
-        """
-        根据第ind步的状态stat，产生新状态，并计算data
-        """
-        fv=self.features(stat)#得到当前状态的特征向量
-
-        stat_info=self.sequence[ind][stat]
-        alphas=stat_info['alphas']
-        #betas=stat_info.setdefault('betas',{})
-        predictors=stat_info['pi']
-        c,v=alphas[0][0],alphas[0]['v']
-        #shift
-        key=self.stats.shift(self.raw,stat)
-        if key:
-            if key not in self.sequence[ind+1]:
-                self.sequence[ind+1][key]={'pi':set(),'alphas':[]}
-            new_stat_info=self.sequence[ind+1][key]
-            new_stat_info['pi'].add((ind,stat))
-            #xi=self.shift_weights(fv)
-            xi=self.actions['s'](fv)
-            new_stat_info['alphas'].append({
-                        0:c+xi,
-                        'v':0,
-                        'a':'s',
-                        'p':((ind,stat),)})
-            #betas['s']=[key]
-        for p_step,predictor in predictors:
-            last_stat_info=self.sequence[p_step][predictor]
-            last_fv=self.features(predictor)
-            #last_xi=self.shift_weights(last_fv)
-            last_xi=self.actions['s'](last_fv)
-            last_c,last_v=last_stat_info['alphas'][0][0],last_stat_info['alphas'][0]['v']
-            lkey,rkey=self.stats.reduce(self.raw,stat,predictor)
-            #left-reduce
-            if lkey :
-                if lkey not in self.sequence[ind+1]:
-                    self.sequence[ind+1][lkey]={'pi':set(),'alphas':[]}
-                new_stat_info=self.sequence[ind+1][lkey]
-                new_stat_info['pi'].update(last_stat_info['pi'])
-                #lamda=self.lreduce_weights(fv)
-                lamda=self.actions['l'](fv)
-                delta=lamda+last_xi
-                new_stat_info['alphas'].append({
-                            0:last_c+v+delta,
-                            'v':last_v+v+delta,
-                            'a':'l',
-                            'p':((ind,stat),(p_step,predictor))})
-                #betas['l']=[lkey]
-
-            #right-reduce
-            if rkey:
-                if rkey not in self.sequence[ind+1]:
-                    self.sequence[ind+1][rkey]={'pi':set(),'alphas':[]}
-                new_stat_info=self.sequence[ind+1][rkey]
-                new_stat_info['pi'].update(last_stat_info['pi'])
-                #rho=self.rreduce_weights(fv)
-                rho=self.actions['r'](fv)
-                delta=rho+last_xi
-                new_stat_info['alphas'].append({
-                            0:last_c+v+delta,
-                            'v':last_v+v+delta,
-                            'a':'r',
-                            'p':((ind,stat),(p_step,predictor))})
-                #betas['r']=[rkey]
-
-
     def make_result(self):
         """
         由alphas中间的记录计算actions
@@ -346,5 +400,4 @@ class Model(perceptrons.Base_Model):
         """
         super(Model,self).__init__(model_file,schema)
         self.codec=codec
-        self.Eval=eval.Eval
 
