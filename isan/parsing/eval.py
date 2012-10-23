c431f729ff7ba6769364a6be3628a313472e55bc
zhangkaixu <zhangkaixu@hotmail.com>
2012-10-23 11:32:17 +0800

    eval for parser contains root and non-root

--- isan/parsing/eval.py
+++ isan/parsing/eval.py
@@ -9,18 +9,29 @@ class Eval:
     def __init__(self):
         self.std=0
         self.cor=0
+        self.non_root_std=0
+        self.non_root_cor=0
+        self.root_std=0
+        self.root_cor=0
         self.start_time=time.time()
     def __call__(self,std_result,rst_result):
-        std_result=[ind for _,_,ind,_ in std_result]
-        self.std+=len(std_result)
-        self.cor+=sum(1 if s==r else 0 for s,r in zip(std_result,rst_result))
+        #std_result=[ind for _,_,ind,_ in std_result]
+        self.std+=sum(1 for s in std_result if s[1]!='PU')
+        for s,r in zip(std_result,rst_result) : 
+            if s[1]=='PU' : continue
+            if s[2]!=-1 :
+                self.non_root_std+=1
+                if s[2]==r : self.non_root_cor+=1
+            else :
+                self.root_std+=1
+                if s[2]==r : self.root_cor+=1
         pass
     def print_result(self):
         duration=time.time()-self.start_time
-        print("std:%d rst:%d 正确率:\033[32;01m%.4f\033[1;m 历时:%.2f 现时:%s"%(
+        print("std:%d non-root正确率:\033[32;01m%.4f\033[1;m root正确率:\033[32;01m%.4f\033[1;m 历时:%.2f 现时:%s"%(
                 self.std,
-                self.cor,
-                self.cor/self.std,
+                self.non_root_cor/self.non_root_std,
+                self.root_cor/self.root_std,
                 duration,
                 time.strftime("%H:%M:%S")))
 
