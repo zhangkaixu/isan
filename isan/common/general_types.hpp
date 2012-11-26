cb238a8a1ec96bc30549213cbdf8dfa9074bea35
zhangkaixu <zhangkaixu@hotmail.com>
2012-11-26 20:35:08 +0800

    mi

--- isan/common/general_types.hpp
+++ isan/common/general_types.hpp
@@ -4,13 +4,48 @@
 #include "isan/common/smart_string.hpp"
 namespace isan{
 typedef long Action_Type;
-class Smart_Chars: public Smart_String<unsigned char>{
+
+template<class ITEM>
+class Smart_String2{
+public:
+    typedef size_t SIZE_T;
+    ITEM* pt;
+    SIZE_T length;
+    SIZE_T* _ref_count;
+    Smart_String2(){
+        pt=NULL;
+        length=0;
+        _ref_count=new SIZE_T();
+        *_ref_count=1;
+    };
+
+
+};
+
+class Smart_Chars: public Smart_String2<unsigned char>{
 public:
     PyObject* pack() const{
         return PyBytes_FromStringAndSize((char*)pt,length);
     };
     Smart_Chars(){
+        pt=NULL;
+        length=0;
+        _ref_count=new SIZE_T();
+        *_ref_count=1;
         //std::cout<<*_ref_count<<"\n";
+    };
+    ~Smart_Chars(){
+        (*_ref_count)--;
+        if(!*_ref_count){
+            delete _ref_count;
+            if(pt)delete[] pt;
+        }
+    };
+    Smart_Chars(const Smart_Chars& other){
+        pt=other.pt;
+        length=other.length;
+        _ref_count=other._ref_count;
+        (*_ref_count)++;
     };
     Smart_Chars(PyObject* py_key){
         char* buffer;
@@ -41,6 +76,48 @@ public:
     };
     inline unsigned char& operator[](const int i) const{
         return pt[i];
+    };
+    class HASH{
+    public:
+        inline SIZE_T operator()(const Smart_String2& cx) const{
+            SIZE_T value=0;
+            for(int i=0;i<cx.length;i++){
+                value+=cx.pt[i]<<((i%8)*8);
+            }
+            return value;
+        }
+    };
+    inline const size_t& size() const{
+        return length;
+    };
+    inline bool operator==(const Smart_Chars&next) const{
+        if(length!=next.length)
+            return false;
+        if(pt==next.pt)return true;
+        for(int i=0;i<length;i++){
+            if(pt[i]!=next.pt[i])return false;
+        }
+        return true;
+    };
+    inline bool operator<(const Smart_Chars& next)const{
+        if(length<next.length)return 1;
+        if(length>next.length)return 0;
+        for(int i=0;i<length;i++){
+            if(pt[i]<next.pt[i])return 1;
+            if(pt[i]>next.pt[i])return 0;
+        }
+        return 0;
+    };
+    inline void operator=(const Smart_Chars& other){
+        (*_ref_count)--;
+        if(!*_ref_count){
+            delete _ref_count;
+            if(pt)delete[] pt;
+        }
+        pt=other.pt;
+        length=other.length;
+        _ref_count=other._ref_count;
+        (*_ref_count)++;
     };
 };
 typedef int Score_Type;
