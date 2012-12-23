effa2c84cb2506b98cc866b205a11c8c09c9c698
zhangkaixu <zhangkaixu@hotmail.com>
2012-12-23 14:48:24 +0800

    ..

--- isan/parsing/default_dep2.py
+++ isan/parsing/default_dep2.py
@@ -37,6 +37,14 @@ class codec:
                     v=[v['dep'][1]]
                 gold.append([k,v])
             return gold
+
+    @staticmethod
+    def arcs_to_result(arcs,lattice):
+        return arcs
+    @staticmethod
+    def result_to_arcs(result):
+        result=[ind for _,_,ind,_ in result]
+        return result
     @staticmethod
     def encode(raw,result):
         return ' '.join(['_'.join([item[0],item[1],str(head)]) for item,head in zip(raw,result)])
@@ -62,17 +70,95 @@ class codec:
 
 class Action :
     _shift_offset=200
-    shift_action=ord('s')
     left_reduce=ord('l')
     right_reduce=ord('r')
+
+    @staticmethod
+    def shift_action(sind):
+        return Action._shift_offset+sind
+    @staticmethod
+    def parse_action(action):
+        if action<Action._shift_offset : 
+            return False,
+        else :
+            return True,action-Action._shift_offset
+
+
+    @staticmethod
+    def actions_to_arcs(actions):
+        ind=0
+        stack=[]
+        arcs=[]
+        for a in actions:
+            is_shift,*rest=Action.parse_action(a)
+            if is_shift :
+                sind=rest[0]
+                stack.append(sind)
+                ind+=1
+            elif a==Action.left_reduce:
+                arcs.append((stack[-1],stack[-2]))
+                stack.pop()
+            elif a==Action.right_reduce:
+                arcs.append((stack[-2],stack[-1]))
+                stack[-2]=stack[-1]
+                stack.pop()
+        arcs.append((stack[-1],-1))
+        arcs.sort()
+        arcs=[x for _,x in arcs]
+        return arcs
+    @staticmethod
+    def arcs_to_actions(arcs):
+        result=arcs
+        stack=[]
+        actions=[]
+        #result=[ind for _,_,ind,_ in result]
+        record=[[ind,head,0] for ind,head in enumerate(result)]# [ind, ind_of_head, 是head的次数]
+        for ind,head,_ in record:
+            if head!=-1 :
+                record[head][2]+=1
+        for ind,head in enumerate(result):
+            #actions.append(self.shift_action)
+            actions.append(Action.shift_action(ind))
+            stack.append([ind,result[ind],record[ind][2]])
+            while len(stack)>=2:
+                if stack[-1][2]==0 and stack[-1][1]!=-1 and stack[-1][1]==stack[-2][0]:
+                    actions.append(Action.left_reduce)
+                    stack.pop()
+                    stack[-1][2]-=1
+                elif stack[-2][1]!=-1 and stack[-2][1]==stack[-1][0]:
+                    actions.append(Action.right_reduce)
+                    stack[-2]=stack[-1]
+                    stack.pop()
+                    stack[-1][2]-=1
+                else:
+                    break
+
+        return actions
+
+
 
 class State(Action) :
     init_stat=pickle.dumps((0,(0,0),(None,None,None)))
     def __init__(self,bt,lattice):
         self.lattice=lattice
         state=pickle.loads(bt)
-        self.ind,self.wspan,self.stack_top=state
+        self.ind,self.span,self.stack_top=state
         self.stop_step=2*len(self.lattice.items)-1
+        pass
+
+    def shift(self,shift_ind):
+        item=self.lattice.items[shift_ind]
+        next_ind=self.ind+1
+        if next_ind==self.stop_step : next_ind=-1
+
+        state=(
+                next_ind,
+                (item[0],item[1]),
+                ((item[2],item[3],None,None),
+                        self.stack_top[0],
+                        self.stack_top[1][1] if self.stack_top[1] else None)
+                )
+        return [(self.shift_action(shift_ind),next_ind,pickle.dumps(state))]
         pass
 
 
@@ -84,50 +170,41 @@ class Dep (Base_Task):
     name="依存句法分析"
     Action=Action
     State=State
-
     
-    shift_action=Action.shift_action
     left_reduce=Action.left_reduce
     right_reduce=Action.right_reduce
-
 
     Eval=eval.Eval
     codec=codec
 
     def shift(self,last_ind,stat):
+        #print(last_ind)
         state=self.State(stat,self.lattice)
+
+        shift_inds=self.lattice.begins.get(state.span[1],[])
+        rtn2=[]
+        for shift_ind in shift_inds:
+            rtn2+=state.shift(shift_ind)
 
         next_ind=last_ind+1 if last_ind+1 <= (2*len(self.lattice.items)-2) else -1
         stat=State.load(stat)
         ind,span,stack_top=stat
+        
         if ind>=len(self.lattice.items): return []
-        #wind=state.ind
-        #print(wind)
-        #if wind<len(self.lattice.items) :
-        #    span1=self.lattice.items[wind][1]
-        #else :
-        #    return []
-        #shift_inds=self.lattice.begins.get(span1,[])
-        #print(shift_inds,ind)
-        #input()
-        #rtn=[]
-        #for shift_ind in shift_inds:
-            
-            #rtn+=state.shift(shift_ind)
-        #return rtn
-
         rtn= [
-                (self.shift_action,next_ind,
+                (self.Action.shift_action(ind),next_ind,
                     pickle.dumps(
                 (ind+1,
-                (ind,ind+1),
-                #(self.lattice.items[ind][0],self.lattice.items[ind][1]),
+                (self.lattice.items[ind][0],self.lattice.items[ind][1]),
                 ((self.lattice.items[ind][2],self.lattice.items[ind][3],None,None),
                         stack_top[0],
                         stack_top[1][1] if stack_top[1] else None)
                 )))
                 ]
-
+        #print(rtn2)
+        #print(rtn)
+        #if rtn2!=rtn :
+        #    input()
         return rtn
 
     def reduce(self,last_ind,stat,pred_inds,predictors):
@@ -172,7 +249,11 @@ class Dep (Base_Task):
         fvs=[]
         fv=self.gen_features_one(span)
         for action in actions:
-            action=chr(action).encode()
+            is_shift,*_=self.Action.parse_action(action)
+            if is_shift :
+                action='s'.encode()
+            else :
+                action=chr(action).encode()
             fvs.append([action+x for x in fv])
             #print(sorted(fvs[-1]))
         #input()
@@ -244,79 +325,26 @@ class Dep (Base_Task):
         #print(*[x.decode() for x in fv])
         #input()
         return fv
-    def moves_to_result(self,moves,_):
-        #actions=moves[1]
-        actions=list(zip(*moves))[2]
-        ind=0
-        stack=[]
-        arcs=[]
-        for a in actions:
-            if a==self.shift_action:
-                stack.append(ind)
-                ind+=1
-            elif a==self.left_reduce:
-                arcs.append((stack[-1],stack[-2]))
-                stack.pop()
-            elif a==self.right_reduce:
-                arcs.append((stack[-2],stack[-1]))
-                stack[-2]=stack[-1]
-                stack.pop()
-        arcs.append((stack[-1],-1))
-        arcs.sort()
-        arcs=[x for _,x in arcs]
-        return arcs
-    def result_to_actions(self,result):
-        """
-        将依存树转化为shift-reduce的动作序列（与动态规划用的状态空间无关）
-        在一对多中选择了一个（没搞清楚相关工作怎么弄的）
-        """
-        stack=[]
-        actions=[]
-        result=[ind for _,_,ind,_ in result]
-        record=[[ind,head,0] for ind,head in enumerate(result)]# [ind, ind_of_head, 是head的次数]
-        for ind,head,_ in record:
-            if head!=-1 :
-                record[head][2]+=1
-        for ind,head in enumerate(result):
-            actions.append(self.shift_action)
-            stack.append([ind,result[ind],record[ind][2]])
-            while len(stack)>=2:
-                if stack[-1][2]==0 and stack[-1][1]!=-1 and stack[-1][1]==stack[-2][0]:
-                    actions.append(self.left_reduce)
-                    stack.pop()
-                    stack[-1][2]-=1
-                elif stack[-2][1]!=-1 and stack[-2][1]==stack[-1][0]:
-                    actions.append(self.right_reduce)
-                    stack[-2]=stack[-1]
-                    stack.pop()
-                    stack[-1][2]-=1
-                else:
-                    break
-
-        return actions
+    
     def actions_to_stats(self,raw,actions):
-        stat=None
-        stack=[]# [ w,t,l_t,r_t , span[0],span[1]]
-        ind=0
-        for action in actions:
-            stat=(ind,(0,0)if not stack else (stack[-1][4],stack[-1][5]),(tuple(stack[-1][:4]) if len(stack)>0 else None,
-                tuple(stack[-2][:4]) if len(stack)>1 else None,
-                        stack[-3][1] if len(stack)>2 else None,
-                        ))
-            yield pickle.dumps(stat)
-            if not (action==self.left_reduce or action==self.right_reduce) :
-                stack.append([self.lattice.items[ind][2],self.lattice.items[ind][3],None,None,ind,ind+1])
-                ind+=1
+        stack=[[0,self.State.init_stat]]#准备好栈
+        stats=[]#状态序列
+        for action in actions :
+            stats.append([stack[-1][0],stack[-1][1]]) #状态
+            is_shift,*rest=self.Action.parse_action(action)#解析动作
+            if is_shift : # shift动作
+                sind=rest[0] # 得到shift的对象
+                nexts=self.shift(stack[-1][0],stack[-1][1])
+                n=[n for n in nexts if n[0]==action][0]
+                stack.append([n[1],n[2]])
             else :
-                if action==self.left_reduce :
-                    stack[-2][3]=stack[-1][1]
-                    stack[-2][5]=stack[-1][5]
-                    stack.pop()
-                if action==self.right_reduce :
-                    stack[-1][2]=stack[-2][1]
-                    stack[-1][4]=stack[-2][4]
-                    stack[-2]=stack[-1]
-                    stack.pop()
+                nexts=self.reduce(stack[-1][0],stack[-1][1],[stack[-2][0]],[stack[-2][1]])
+                n=[n for n in nexts if n[0]==action][0]
+                stack.pop()
+                stack.pop()
+                stack.append([n[1],n[2]])
+        stats=[x[1] for x in stats]
+        return stats
 
     ## stuffs about the early update
     def set_oracle(self,raw,y) :
