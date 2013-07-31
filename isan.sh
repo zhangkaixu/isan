dd493b875f536177e41f6b3ec07b16b220390852
zhangkaixu <zhangkaixu@hotmail.com>
2013-07-31 13:05:09 +0800

    interfac

--- isan.sh
+++ isan.sh
@@ -13,6 +13,59 @@ if [ $1 = 'link' ] ; then
     ln -s ${src}/isan.sh .
 fi
 
+
+if [ $1 = 'stack' ] ; then
+    src=$2
+    batch=$3 # batchsize
+    fold=$4
+    dst=$5
+    for tgt in `seq 0 $(expr $fold - 1 ) `; do
+        cat $src | awk "(NR-NR%${batch})/${batch}%${fold}==${tgt} {print}" > ${dst}/${tgt}.test
+        cat $src | awk "(NR-NR%${batch})/${batch}%${fold}!=${tgt} {print}" > ${dst}/${tgt}.train
+    done;
+    
+    exit
+fi
+
+if [ $1 = 'shuffle' ] ; then
+    cmd=$0
+    shift
+    if [ $# = 0 ]; then
+        echo "usage:"
+        echo "    " ${cmd} shuffle -p processor-number=1 -m model-number=1 -d dir=. blabla
+        exit
+    fi
+    dst='.' # dir
+    nm='1' # number of models
+    np='1'
+    while [ `echo $1 | grep '\-'` ] ; do
+        if [ `echo $1 | grep '\-p'` ] ; then
+            np=$2
+            shift;shift
+        fi
+        if [ `echo $1 | grep '\-d'` ] ; then
+            dst=$2
+            shift;shift
+        fi
+        if [ `echo $1 | grep '\-m'` ] ; then
+            nm=$2
+            shift;shift
+        fi
+    done
+    echo "train [\033[34m$nm\033[0m] model(s)" "into [\033[34m$dst\033[0m]"
+    echo "using [\033[34m$np\033[0m] processor(s)"
+    echo "the command line is:\033[34m${cmd} $*\033[0m"
+
+    #exit
+
+    mkdir $dst -p
+    echo `for i in $(seq $nm); do echo "${dst}/model.$i.gz --seed $i"; done` | xargs  -n 3 -P $np ${cmd} $*
+    ${cmd} $1 ${dst}/model.gz --append_model `for i in $(seq $nm); do echo "${dst}/model.$i.gz"; done`
+    exit
+fi
+
+
+
 #
 # 中文分词模型
 #
